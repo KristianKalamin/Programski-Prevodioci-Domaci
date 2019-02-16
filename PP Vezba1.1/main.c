2fdcc3278e1f298bd889c0e2b55ddcae1709717f
Kristian Kalamin <kalamin.michal@gmail.com>
2019-02-16 17:16:48 +0100

    Zavrsen zadatak

--- PP Vezba1.1/main.c
+++ PP Vezba1.1/main.c
@@ -1,8 +1,136 @@
+#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+#define TRUE 1
+#define FALSE 0
+
+int isDot(int x)
 {
-    printf("Hello world!\n");
+    if (x == 46)
+        return TRUE;
+    return FALSE;
+}
+
+int whiteSpace(int x)
+{
+    if (x == 32 || x == 9 || x == 10 || x == '\000' || x == EOF)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+int isIllegalChar(int x)
+{
+    if ((x >= 65 && x <= 90) || (x >= 97 && x <= 122) || x == 46)
+        return FALSE;
+    else
+        return TRUE;
+}
+
+int checkInputChar(FILE *f)
+{
+    char c, d;
+    while ((c = getc(f)) != EOF)
+    {
+
+        if (isIllegalChar(c) && !whiteSpace(c))
+        {
+            printf("\nGRESKA: %c", c);
+            return FALSE;
+        }
+        d = c;
+    }
+    if (!isDot(d))
+    {
+        printf("\nGreska recenica mora da se zavrsi sa tackom!\n");
+        return FALSE;
+    }
+    return TRUE;
+}
+
+void findDotPosition(FILE *f, int *startOfWord)
+{
+    for (int i = 0;; i++)
+    {
+        if ((isDot(f->_ptr[i])) || (f->_ptr[i] == '\000'))
+        {
+            while ((!whiteSpace(f->_ptr[i])) && (i > 0))
+            {
+                i--;
+            }
+            (*startOfWord) = i;
+            return;
+        }
+    }
+}
+
+FILE *printStartWord(FILE *f)
+{
+    char c;
+    printf("\nRecenica pocinje sa: ");
+    while ((c = getc(f)) != EOF)
+    {
+        if (whiteSpace(c))
+            break;
+        else if (isDot(c))
+        {
+            ungetc(c, f);
+            break;
+        }
+        else
+            printf("%c", c);
+    }
+
+    return f;
+}
+
+FILE *printEndWord(FILE *f, int *start)
+{
+    char c;
+    printf("\nRecenica se zavrsava sa: ");
+    while ((c = getc(f)) != EOF)
+    {
+        if (isDot(c))
+        {
+            //ungetc(c,f);
+            break;
+        }
+        else
+            printf("%c", c);
+    }
+
+    return f;
+}
+
+int main(/*int argc, char *argv[]*/)
+{
+    FILE *errorChar = fopen("test.txt" /*argv[1]*/, "r");
+
+    if (checkInputChar(errorChar))
+    {
+        FILE *f = fopen("test.txt" /*argv[1]*/, "r");
+        char c;
+        int startOfWord = 0;
+
+        f = printStartWord(f);
+
+        while ((c = getc(f)) != EOF)
+        {
+            ungetc(c, f);
+            findDotPosition(f, &startOfWord);
+            for (int i = 0; i <= startOfWord; i++)
+                c = getc(f);
+
+            if (startOfWord > 0)
+                f = printEndWord(f, &startOfWord);
+              if (isDot(c) || whiteSpace(c))
+                c = getc(f); // to skip dot
+            if (c == EOF)
+                break;
+            else
+                f = printStartWord(f);
+        }
+    }
+
     return 0;
 }
