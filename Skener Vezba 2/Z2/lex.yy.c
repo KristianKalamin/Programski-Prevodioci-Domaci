e71c43a2d57eec5d392133de22ef31e4db1e5dea
abc <kalamin.michal@gmail.com>
2019-03-03 11:14:01 -0800

    Ispravljeno

--- Skener Vezba 2/Z2/lex.yy.c
+++ Skener Vezba 2/Z2/lex.yy.c
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 5
-#define YY_END_OF_BUFFER 6
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,10 +361,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[15] =
+static const flex_int16_t yy_accept[13] =
     {   0,
-        0,    0,    0,    0,    6,    5,    5,    2,    2,    1,
-        4,    3,    4,    0
+        0,    0,    4,    3,    3,    0,    2,    0,    0,    2,
+        1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -401,31 +401,31 @@ static const YY_CHAR yy_ec[256] =
 
 static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    2,    1,    3
+        1,    2,    1,    1
     } ;
 
-static const flex_int16_t yy_base[18] =
+static const flex_int16_t yy_base[15] =
     {   0,
-       11,   10,    0,    0,   13,   16,    2,   16,    0,   16,
-        0,   16,    0,   16,    6,    8,    9
+       10,    9,   12,   15,    0,    4,    0,    0,    7,    0,
+        0,   15,    8,    0
     } ;
 
-static const flex_int16_t yy_def[18] =
+static const flex_int16_t yy_def[15] =
     {   0,
-       15,   15,   14,    3,   14,   14,   14,   14,   16,   14,
-       17,   14,   17,    0,   14,   14,   14
+       13,   13,   12,   12,   12,   12,   14,    6,    6,   14,
+        6,    0,   12,   12
     } ;
 
-static const flex_int16_t yy_nxt[21] =
+static const flex_int16_t yy_nxt[20] =
     {   0,
-        8,    8,    9,    8,   10,   11,    6,    6,    6,   13,
-       12,   13,   14,    7,    7,    5,   14,   14,   14,   14
+       10,   12,    6,    7,    8,    8,    9,    8,    4,    4,
+       11,   12,    5,    5,    3,   12,   12,   12,   12
     } ;
 
-static const flex_int16_t yy_chk[21] =
+static const flex_int16_t yy_chk[20] =
     {   0,
-        3,    3,    3,    3,    7,    7,   15,   15,   15,   17,
-       16,   17,    5,    2,    1,   14,   14,   14,   14,   14
+       14,    0,    5,    5,    6,    6,    6,    6,   13,   13,
+        9,    3,    2,    1,   12,   12,   12,   12,   12
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -443,11 +443,9 @@ int yy_flex_debug = 0;
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
 #line 1 "comment.l"
-
-#line 448 "lex.yy.c"
+#line 447 "lex.yy.c"
 
 #define INITIAL 0
-#define COMMENT 1
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -662,9 +660,10 @@ YY_DECL
 		}
 
 	{
-#line 3 "comment.l"
+#line 1 "comment.l"
 
-#line 668 "lex.yy.c"
+
+#line 667 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -691,13 +690,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 15 )
+				if ( yy_current_state >= 13 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 16 );
+		while ( yy_base[yy_current_state] != 15 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -722,34 +721,23 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
+/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 4 "comment.l"
-{ BEGIN COMMENT;}
+#line 3 "comment.l"
+
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
 #line 5 "comment.l"
-{/* preskace tekst komentara */}
+
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 6 "comment.l"
-{BEGIN INITIAL;}
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
 #line 8 "comment.l"
-
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 11 "comment.l"
 ECHO;
 	YY_BREAK
-#line 751 "lex.yy.c"
+#line 740 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
-case YY_STATE_EOF(COMMENT):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1045,7 +1033,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 15 )
+			if ( yy_current_state >= 13 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1073,11 +1061,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 15 )
+		if ( yy_current_state >= 13 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 14);
+	yy_is_jam = (yy_current_state == 12);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1753,4 +1741,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 11 "comment.l"
+#line 8 "comment.l"
